9b7d564fc195362b6ac459757cba0a0e7d3c60fd
Софья Псарева <sofap2711@gmail.com>
2026-04-25 14:59:36 +0300

    feat: add file utils functions

--- ai-diploma/src/main.py
+++ ai-diploma/src/main.py
@@ -1,5 +1,7 @@
-def main():
-    print("Hello from clean project!")
+from file_utils import save_text, load_text, append_text, count_lines
 
-if __name__ == "__main__":
-    main()
+save_text("project_note.txt", "Это первая строка проекта.")
+append_text("project_note.txt", "Это вторая строка проекта.")
+
+print(load_text("project_note.txt"))
+print("Количество строк:", count_lines("project_note.txt"))
